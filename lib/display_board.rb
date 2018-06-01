fc629cefb0c559551a3b6aa0324ba3db711d5c70
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 20:31:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board()
-  
+def display_board(markers)
+  row_1 = []
   div = "\n" + "-"*11 + "\n"
 end
