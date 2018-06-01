8da7dadc5930a07e768b55f0aa25547c70deef08
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 20:44:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(markers)
-  row_1 = markers[0..2][" #{markers[]} "].join("|")
   div = "\n" + "-"*11 + "\n"
+  rows = (0..8).step(3).collect{|i| markers[i..i+2].collect{|x| " #{x} "}.join("|")}
+  puts rows.join(div)
 end
